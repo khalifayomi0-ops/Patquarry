807c224cb15e998cf122e5f96c65f0483bd24b09
khalifayomi0-ops <khalifayomi0@gmail.com>
2026-08-12 20:24:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 const $ = (selector) => document.querySelector(selector);
 const app = $("#app");
 
-const SUPABASE_URL = "https://exiuegfihnekrizbwdkr.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_YhpuBvMrRJtA_DDDT-xHsQ_0jce1uJT";
+const SUPABASE_URL = "https://https://oxywfbjaubqjobwtuulx.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_iuaOrTmIlqRYx8X8uLlNkg_CNjUcZW9";
 
 let supabaseClient = null;
 let me = null;
